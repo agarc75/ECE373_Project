58a8f67f21cbbf14c93f0500129efff02f109ca1
rigoaa <rigo.avila70@gmail.com>
2017-11-14 02:13:33 -0700

    Added new user interface

--- LoginGUI.java
+++ LoginGUI.java
@@ -42,7 +42,7 @@ public class LoginGUI extends JFrame
 		JPanel turnsLogin = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
-		c.fill = GridBagConstraints.HORIZONTAL;
+		
 		
 		//Text Fields
 		JLabel userNameLabel = new JLabel("Username: ");
@@ -59,7 +59,7 @@ public class LoginGUI extends JFrame
 		
 		
 		c.ipady = 5;
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
 		
 		c.weightx = 2;
 		c.gridx = 0;
@@ -80,9 +80,12 @@ public class LoginGUI extends JFrame
 		c.gridx = 1;
 	    c.gridy = 1;
 		turnsLogin.add(passwordField, c);
-		
-		c.weightx = 0.5;
-		c.gridx = 0;
+	
+		c.fill = GridBagConstraints.LINE_END;
+		c.weightx = 3;
+		c.ipady = 3;
+		c.ipadx = 15;
+		c.gridx = 1;
 	    c.gridy = 2;
 		turnsLogin.add(loginButton, c);
 		
@@ -110,9 +113,9 @@ public class LoginGUI extends JFrame
 			if(source.equals(forgotPasswordLabel))
 				System.out.println("Forgot Password?");
 			if(source.equals(newUserLabel))
-				System.out.println("Welcome!");
+				NewUserGUI();
 		}
-
+		
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			//TODO
@@ -135,9 +138,86 @@ public class LoginGUI extends JFrame
 			// TODO Auto-generated method stub
 			
 		}
-
+	}
+	
+	private void NewUserGUI() 
+	{
+		JLabel welcomeMessage = new JLabel("Create an Account");
+		JLabel nameLabel = new JLabel("Name: ");
+		JTextField nameField = new JTextField(10);
+		JLabel userNameLabel = new JLabel("Username: ");
+		JTextField userNameField = new JTextField(10);
+		JLabel emailLabel = new JLabel("Email: ");
+		JTextField emailField = new JTextField(10);
+		JLabel passwordLabel = new JLabel("Password");
+		JTextField passwordField = new JTextField(10);
+		JLabel confirmLabel = new JLabel("Confirm Password: ");
+		JTextField confirmField = new JTextField(10);
+		
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 0;
+		panel.add(welcomeMessage, c);
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 1;
+		panel.add(nameLabel, c);
+		c.weightx = 2;
+		c.gridx = 1;
+	    c.gridy = 1;
+		panel.add(nameField, c);
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 2;
+		panel.add(userNameLabel, c);
+		c.weightx = 2;
+		c.gridx = 1;
+	    c.gridy = 2;
+		panel.add(userNameField, c);
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 3;
+		panel.add(emailLabel, c);
+		c.weightx = 2;
+		c.gridx = 1;
+	    c.gridy = 3;
+		panel.add(emailField, c);
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 4;
+		panel.add(passwordLabel, c);
+		c.weightx = 2;
+		c.gridx = 1;
+	    c.gridy = 4;
+		panel.add(passwordField, c);
+		
+		c.weightx = 2;
+		c.gridx = 0;
+	    c.gridy = 5;
+		panel.add(confirmLabel, c);
+		c.weightx = 2;
+		c.gridx = 1;
+	    c.gridy = 5;
+		panel.add(confirmField, c);
+		
+		JOptionPane.showMessageDialog(null, panel, "Welcome to Turns!", -1);
 		
 	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
