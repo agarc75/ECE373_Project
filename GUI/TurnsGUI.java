9a43046ae6970181c45c525385f349547053ac91
rigoaa <rigo.avila70@gmail.com>
2017-11-25 01:55:06 -0700

    Login gui now returns bool to turns gui

--- GUI/TurnsGUI.java
+++ GUI/TurnsGUI.java
@@ -1,13 +1,25 @@
 package GUI;
 
 import framework.Turns;
+import framework.User;
 
 public class TurnsGUI
 {
-	private static Turns turn;
+	
 	
 	public static void main(String[] arg)
-	{
+	{	
+		//Test user
+		Turns turn = new Turns();
+		User ravila = new User("Rigo Avila", "ravila", "ravila@email.com", "1234");
+		turn.addUser("ravila", ravila);
+		
 		new LoginGUI(turn);
+		
+		//Returns true if user logins in or creates account successfully
+		if(LoginGUI.getCorrectUser()) {
+			System.out.println("Welcome User");
+		}
+		
 	}
 }
