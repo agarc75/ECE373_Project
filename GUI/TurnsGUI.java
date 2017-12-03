4bcb87b3fa4e3a0498495e17567f21ff45346019
rigoaa <rigo.avila70@gmail.com>
2017-12-02 21:36:28 -0700

    Dynamic size settings for various items

--- GUI/TurnsGUI.java
+++ GUI/TurnsGUI.java
@@ -18,40 +18,46 @@ public class TurnsGUI
 		User user3 = new User("Aaron", "themyth", "aaron@aol.com", "1357");
 		User user4 = new User();
 		User user5 = new User();
+		User user6 = new User();
+		User user7 = new User();
+		
 		
 		//test task
-		Task task1 = new Task("Trash", user1);
-		Task task2 = new Task("Stuff1", user1);
-		Task task3 = new Task("Stuff2", user1);
-		Task task4 = new Task("Stuff3", user1);
-		Task task5 = new Task("Stuff3", user1);
-		Task task6 = new Task("Stuff3", user1);
+		Task task1 = new Task("Stuff1", user1);
+		Task task2 = new Task("Stuff2", user1);
+		Task task3 = new Task("Stuff3", user1);
+		Task task4 = new Task("Stuff4", user1);
+		Task task5 = new Task("Stuff5", user1);
 		Task task7 = new Task("Stuff3", user1);
 		Task task8 = new Task("Stuff3", user1);
 		Task task9 = new Task("Stuff3", user1);
 		Task task10 = new Task("Stuff3", user1);
 		Task task11 = new Task("Stuff4", user1);
-		Task task12 = new Task("Stuff5", user1);
+//		Task task12 = new Task("Stuff5", user1);
 		
 		
 		
 		task1.addUser(user2);
-		task1.addUser(user3);
 		
 		task2.addUser(user2);
 		task2.addUser(user3);
-		task2.addUser(user4);
-		task2.addUser(user5);
 		
 		task3.addUser(user2);
 		task3.addUser(user3);
+		task3.addUser(user4);
 		
-		task4.addUser(user2);
 		task4.addUser(user3);
+		task4.addUser(user2);
+		task4.addUser(user4);
+		task4.addUser(user5);
+		
 		task5.addUser(user2);
 		task5.addUser(user3);
-		task6.addUser(user2);
-		task6.addUser(user3);
+		task5.addUser(user4);
+		task5.addUser(user5);
+		task5.addUser(user6);
+		task5.addUser(user7);
+		
 		task7.addUser(user2);
 		task7.addUser(user3);
 		task8.addUser(user2);
@@ -62,8 +68,8 @@ public class TurnsGUI
 		task10.addUser(user3);
 		task11.addUser(user2);
 		task11.addUser(user3);
-		task12.addUser(user2);
-		task12.addUser(user3);
+//		task12.addUser(user2);
+//		task12.addUser(user3);
 		
 		
 		turn.addUser("ravila", user1);
@@ -74,7 +80,7 @@ public class TurnsGUI
 		new LoginGUI(turn);
 		
 		//Bypasses login for testing
-		//turn.setCurrentUSer(user1);
+		//turn.setCurrentUSer(user2);
 		//new MainScreenGUI(turn);
 		
 		
