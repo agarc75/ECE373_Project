4f7b3259200bdb0aec6445280d53bdba88c1ae5e
rigoaa <rigo.avila70@gmail.com>
2017-12-05 08:23:16 -0700

    modified:   GUI/TurnsGUI.java

--- GUI/TurnsGUI.java
+++ GUI/TurnsGUI.java
@@ -23,59 +23,63 @@ public class TurnsGUI
 		//Test user
 		Turns turn = new Turns();
 		User user1 = new User("Rigo Avila", "ravila", "1", "1234");
-		User user2 = new User("Aza Cordova", "elguapo", "aza@gmail.com", "2468");
-		User user3 = new User("Aaron Garcia", "themyth", "aaron@aol.com", "1357");
-		User user4 = new User("Hello World", "thelegend", "test@aol.com", "1234");
-		User user5 = new User();
-		User user6 = new User();
+		User user2 = new User("Aza Cordova", "elguapo", "aza@gmail.com", "123456");
+		User user3 = new User("Aaron Garcia", "themyth", "aaron@aol.com", "123456");
+		User user4 = new User("Kevin Fish", "thelegend", "test@aol.com", "123456");
+		User user5 = new User("Guy Person", "guy", "guy@gmail.com", "123456");
+		User user6 = new User("Roger Jimenez", "rogs", "roger@gmail.com", "123456");
 		User user7 = new User("pleb", "pleb", null, null);
 		
 		
 		//test task
-		Task task1 = new Task("Stuff1", user1);
-		Task task2 = new Task("Stuff2", user1);
-		Task task3 = new Task("Stuff3", user1);
-		Task task4 = new Task("Stuff4", user1);
-		Task task5 = new Task("Stuff5", user1);
-		Task task7 = new Task("Stuff3", user1);
-		Task task8 = new Task("Stuff3", user1);
-		Task task9 = new Task("Stuff3", user1);
-		Task task10 = new Task("Stuff3", user1);
-		Task task11 = new Task("Stuff4", user1);
-		Task task12 = new Task("Stuff5", user1);
+		Task task1 = new Task("Trask Rotation", user1);
+		Task task2 = new Task("Dish Rotation", user1);
+		Task task3 = new Task("Copy Paper", user1);
+		Task task4 = new Task("Finalize Budget", user1);
+		Task task5 = new Task("Can't Delete", user2);
+		Task task7 = new Task("Not your turn", user2);
+		Task task8 = new Task("More than 5", user1);
+		Task task9 = new Task("Stuff4", user1);
+		Task task10 = new Task("Stuff5", user1);
+		Task task11 = new Task("Stuff6", user1);
+		Task task12 = new Task("Stuff7", user1);
 		
 		
 		
 		task1.addUser(user2);
-		task1.nextUser();
+		task1.addUser(user3);
+		task1.addUser(user4);
+		task1.addUser(user5);
 		task1.setDueDate(new GregorianCalendar(2017, 12, 4));
 		
 		task2.addUser(user2);
 		task2.addUser(user3);
+		task2.addUser(user4);
 		
 		task3.addUser(user2);
 		task3.addUser(user3);
-		task3.addUser(user4);
-		task3.nextUser();
 		
 		task4.addUser(user2);
-		task4.addUser(user3);
-		task4.addUser(user4);
-		task4.addUser(user5);
-		task4.nextUser();
-		task4.nextUser();
 		
-		task5.addUser(user2);
+		task5.addUser(user1);
 		task5.addUser(user3);
 		task5.addUser(user4);
 		task5.addUser(user5);
-		task5.addUser(user6);
-		task5.addUser(user7);
 		
 		task7.addUser(user2);
 		task7.addUser(user3);
+		task7.addUser(user4);
+		task7.addUser(user5);
+		
+		
 		task8.addUser(user2);
 		task8.addUser(user3);
+		task8.addUser(user4);
+		task8.addUser(user5);
+		task8.addUser(user6);
+		task8.addUser(user7);
+		
+		
 		task9.addUser(user2);
 		task9.addUser(user3);
 		task10.addUser(user2);
@@ -89,6 +93,9 @@ public class TurnsGUI
 		turn.addUser("ravila", user1);
 		turn.addUser("elguapo", user2);
 		turn.addUser("themyth", user3);
+		turn.addUser("thelegend", user4);
+		turn.addUser("guy", user5);
+		turn.addUser("rogs", user6);
 		turn.addUser("pleb", user7);
 		
 		user1.addFriend(user2);
@@ -110,6 +117,12 @@ public class TurnsGUI
 		group1.addMember(user2);
 		group1.addMember(user3);
 		group1.addMember(user4);
+		group1.addMember(user5);
+		
+		group2.addMember(user2);
+		group2.addMember(user3);
+		group2.addMember(user4);
+		group3.addMember(user5);
 		
 		
 		//Loads data on startup
